f6c3084b4afef24589a8e04c433484faced2df3e
Sebastian San Blas <sebastiansanblas@gmail.com>
2021-05-28 21:57:52 -0300

    Initial Commit

---
+++ SI Converter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SI Converter
+//
+//  Created by Sebastian San Blas on 28/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
